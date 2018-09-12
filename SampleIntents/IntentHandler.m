6c8a271f9405f7c8a57ef0fee197582618296bf8
Ryouji Uematsu <ryouji.uematsu@access-company.com>
2018-09-12 16:00:19 +0900

    some junk

--- SampleIntents/IntentHandler.m
+++ SampleIntents/IntentHandler.m
@@ -29,6 +29,9 @@ NSString *siriInputedData;
     // This is the default implementation.  If you want different objects to handle different intents,
     // you can override this and return the handler you want for that particular intent.
     
+//    if (intent == ) {
+//        
+//    }
     return self;
 }
 
